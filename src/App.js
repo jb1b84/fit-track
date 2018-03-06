abd57b133bad602bb6f41355ba3024f13b179ece
Jason Brown <jason@cratejoy.com>
2018-03-05 23:05:04 -0600

    auth0 setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Navbar, Button } from 'react-bootstrap';
 import './App.css';
 import { LineChart, Line, XAxis, YAxis, Tooltip, ResponsiveContainer } from 'recharts';
 
 class App extends Component {
+  goTo(route) {
+    this.props.history.replace(`/${route}`)
+  }
+
+  login() {
+    this.props.auth.login();
+  }
+
+  logout() {
+    this.props.auth.logout();
+  }
+
   render() {
+    const { isAuthenticated } = this.props.auth;
+
     const data = [
       {date: '1/1/2018', weight: 200},
       {date: '2/1/2018', weight: 180},
@@ -13,8 +27,46 @@ class App extends Component {
 
     return (
       <div className="App">
+        <Navbar fluid>
+          <Navbar.Header>
+            <Navbar.Brand>
+              <a href="/">Auth0 - Fitbit + React</a>
+            </Navbar.Brand>
+            <Button
+              bsStyle="primary"
+              className="btn-margin"
+              onClick={this.goTo.bind(this, 'home')}
+            >
+              Home
+            </Button>
+            {
+              !isAuthenticated() && (
+                <Button
+                  id="qsLoginBtn"
+                  bsStyle="primary"
+                  className="btn-margin"
+                  onClick={this.login.bind(this)}
+                >
+                  Log In
+                </Button>
+              )
+            }
+            {
+              isAuthenticated() && (
+                <Button
+                  id="qsLogoutBtn"
+                  bsStyle="primary"
+                  className="btn-margin"
+                  onClick={this.logout.bind(this)}
+                >
+                  Log Out
+                </Button>
+              )
+            }
+          </Navbar.Header>
+        </Navbar>
+
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Fit tracking</h1>
         </header>
         <p className="App-intro">
@@ -28,6 +80,8 @@ class App extends Component {
             <Tooltip />
           </LineChart>
         </ResponsiveContainer>
+
+
       </div>
     );
   }
---
+++ src/Auth/auth0-variables.js
@@ -0,0 +1,5 @@
+export const AUTH_CONFIG = {
+  domain: 'jpbrown.auth0.com',
+  clientId: 'bKaoy5jgVJ65de2p7yYvsfRl1WoZmH50',
+  callbackUrl: 'http://localhost:3000/callback'
+}
